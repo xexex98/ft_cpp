90543e2c8350088e512ce56abf7db76cb30b93ec
xexex98 <xexex98@gmail.com>
2022-06-02 22:26:17 +0300

    ex03 start

--- Day01/ex00/megaphone.cpp
+++ Day01/ex00/megaphone.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 #include <string>
 #include <ctype.h>
-using namespace std;
+
 
 int main(int argc, char **argv) {
 
 	char c;
 	
 	if (argc <= 1) {
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	}
 	for (int i = 1; i < argc; i++) {
 		int j = 0;
 		while (argv[i][j]) {
-			c = toupper(argv[i][j]);
+			c = std::toupper(argv[i][j]);
 			argv[i][j] = c;
 			j++;
 		}
-		cout << argv[i] << "";
+		std::cout << argv[i] << "";
 	}
-	cout << endl;
+	std::cout << std::endl;
 	return (0);
 }
