5b03abce6cafe4d37408b75bcde3105f34531417
xexex98 <xexex98@gmail.com>
2022-06-14 00:27:52 +0300

    (((

--- Day02/ex04/main.cpp
+++ Day02/ex04/main.cpp
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -6,7 +7,7 @@
 /*   By: mbarra <mbarra@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 22:03:58 by mbarra            #+#    #+#             */
-/*   Updated: 2022/06/13 23:03:13 by mbarra           ###   ########.fr       */
+/*   Updated: 2022/06/14 00:18:53 by mbarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +15,95 @@
 #include <string>
 #include <fstream>
 
-void ft_copy(std::string infile, std::string s1, std::string s2)
-{
-		while(!infile.eof()){
-			std::string line;
-			infile >> line;
-			outfile << (line == s1 ? s2 : line) << " ";
-	}
-	return ;
-}
+// void ft_copy(std::ifstream infile, std::string s1, std::string s2)
+// {
+// 	// std::string name = "as";
+// 	std::ofstream outfile("as.replaced");
+
+// 	while(!infile.eof()){
+// 		std::string line;
+// 		infile >> line;
+// 		outfile << (line == s1 ? s2 : line) << " ";
+// 	}
+// 	infile.close();
+// 	outfile.close();
+// }
+
+// int main(int argc, char **argv)
+// {
+// 	if(argc != 4){
+// 		std::cout << "Invalid input!" << std::endl;
+// 		return(-1);
+// 	}
+// 	std::string s1 = argv[2];
+// 	std::string s2 = argv[3];
+// 	std::ifstream infile(argv[1]);
+// 	if(!infile){
+// 		std::cout << "No read file!" << std::endl;
+// 		return (-2);
+// 	}
+
+// 	// void ft_copy(std::ifstream, std::string s1, std::string s2);
+// 	std::ofstream outfile("as.replaced");
+
+// 	while(!infile.eof()){
+// 		std::string line;
+// 		infile >> line;
+// 		std::string len = line;
+// 		std::cout << line << " ";
+// 		if(line[std::strlen(len) + 1] == '\n')
+// 		outfile << (line == s1 ? s2 : line) << " ";
+// 	}
+// 	infile.close();
+// 	outfile.close();
+// 	return (0);
+// }
+
 
 int main(int argc, char **argv)
 {
 	if(argc != 4){
 		std::cout << "Invalid input!" << std::endl;
-		return(-2);
+		return(-1);
 	}
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
 	std::ifstream infile(argv[1]);
 	if(!infile){
 		std::cout << "No read file!" << std::endl;
-		return (-1);
+		return (-2);
 	}
 
-	std::string s1 = argv[2];
-	std::string s2 = argv[3];
-	std::string name = argv[1];
-	std::ofstream outfile(name + ".replaced");
+	// void ft_copy(std::ifstream, std::string s1, std::string s2);
+	std::ofstream outfile("as.replaced");
+
+	// while(!infile.eof()){
+	// 	std::string line;
+	// 	infile >> line;
+	// 	std::string len = line;
+	// 	std::cout << line << " ";
+	// 	if(line[std::strlen(len) + 1] == '\n')
+	// 	outfile << (line == s1 ? s2 : line) << " ";
+	// }
+	std::string text;
 
 
+	while (std::getline(infile, text)){
+		if (text.find("ipsum") != std::string::npos){
+			int findPos = text.findPos("ipsum");
+			text.erase(text[findPos], std::strlen("ipsum"));
+			// for(int i = 0, i < strlen(string); i++){
+			// 	string[i] = s2[i];
+			// }
+			// string[0] = 'a';
+			std::cout << findPos << std::endl;
+			// std::cout << findPos << std::endl;
+		}
+		outfile << text << "";
+
+	}
+
+	
 	infile.close();
 	outfile.close();
 	return (0);
